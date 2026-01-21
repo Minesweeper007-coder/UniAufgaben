ecdc956b46e02101c18bab47c428e6280f2d8a66
Julius-Schlief <juliuscarlschlief@gmail.com>
2026-01-21 16:30:11 +0100

    Erster Push

--- src/Aufgabe1.scala
+++ src/Aufgabe1.scala
@@ -39,26 +39,113 @@ object Aufgabe1 {
     // höheren Zahl darstellt
 
     // Aufgabe d) 032168754
+
+    // Fast komplett e)
     var A_ray = Array.range(0, 9)
     var B_ray = Array(1, 2, 4, 5, 7, 0, 8, 6, 3)
     var C_ray = Array.range(8, -1, -1)
 
     def highar(per1: Array[Int], per2: Array[Int]): Boolean =
-      if maxSort(per1) sameElements per2 then
+      if per2 sameElements maxSort(per2) then
         false
       else
         var max_ind = per2.length - 1
         var temp = 1
-        while per2(max_ind) < per2(max_ind - 1) do
+        while max_ind >= 2 && per2(max_ind) < per2(max_ind - 1) do
           max_ind = max_ind - 1
         temp = per2(max_ind)
         per2(max_ind) = per2(max_ind - 1)
         per2(max_ind - 1) = temp
         true
 
-    println(highar(A_ray, B_ray))
-    for i <- maxSort(A_ray) do
-      print(A_ray(i))
+    //Aufgabe 2 a) s. Blatt
+    // 2b)
+    type Perm = Array[Int]
+    type Cycles = List[List[Int]]
+
+    def cyclesOf(p: Perm): Cycles =
+      val n = p.length
+      var cs: Cycles = List()
+    // Array zum Merken, welche Zahl schon vorkam
+      var cfm = Array.fill(n)(false)
+      for i <- 0 to n - 1 do
+          if !cfm(i) then
+            cfm(i) = true
+            var cycle = List(i)
+            // Zyklusanfang
+            var j = p(i)
+            // Zyklus durchgehen
+            while j != i do
+              cfm(j) = true
+              cycle = cycle ::: List(j)
+              j = p(j)
+              cs = cs ::: List(cycle)
+      return cs
+
+    def printCycles(p: Perm): Unit =
+      for c <- cyclesOf(p)
+        do println(c)
+
+    var D_ray: Array[Int] = Array.range(0,9)
+    var E_ray: Array[Int] = Array(0,3,2,1,5,8,7,6,4)
+    var F_ray: Array[Int] = Array(0,3,5,1,8,6,2,4,7)
+
+
+    // Einzeln stehende Gruppen werden nicht ausgegeben
+
+
+    var testo: Cycles = List(List(1,3),List(4,5),List(4,5,8),List(6,7))
+
+    def clean(cs: Cycles): Cycles =
+      cs.filter { l =>
+        val count = cs.count(e => e.containsSlice(l))
+        count < 2
+      }
+
+    def permByCycles(cs: Cycles): Perm =
+      var temp:Int = 0
+      def maxEl(cs: Cycles): Int =
+        var max: Int = 0
+        for l <- cs do
+          for e <- l do
+            if e > max then
+              max = e
+        return max
+      var p = Array.range(0,maxEl(cs)+1)
+      for element <- cs do
+        var len:Int = element.length
+        val first:Int = p(element.head)
+        var i = 0
+        while i < len -1 do
+          p(element(i)) = p(element(i+1))
+          i += 1
+        p(element.last) = first
+      return p
+/*
+    for i <- permByCycles(clean(cyclesOf(E_ray))) do
+      print(i)
+*/
+    // Aufgabe 3a)
+    val Ub_ray: Array[Int] = Array(0, 1, 2, 3, 4, 5, 6, 7, 8)
+    val P = Array(7, 3, 0, 2, 5, 1, 6, 4)
+    val Q = Array(5, 2, 4, 6, 0, 3, 1, 7)
+
+    def spiegleF(p: Perm): Perm =
+      var res = Array.fill(8)(0)
+      for i <- 0 to 7 do
+        res(p(i)) = i
+      return res
+
+    for i <- spiegleF(Q) do
+      print(i)
+
+    // Aufgabe 3b)
+    def lexKleiner(p:Perm,q:Perm):Boolean =
+      if p.mkString.toInt > q.mkString.toInt then
+        true
+      else
+        false
+
 
 
   }
